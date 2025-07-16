9ac2d67a24773624d6d9ebb3b251f40788836097
Devika Rajeev <145443027+devikapothiyil@users.noreply.github.com>
2025-07-17 05:22:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import pandas
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
 phonetic_dict = { row.letter:row.code for (index, row) in data.iterrows() }
-word = input("Enter a word:").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+
+def phonetic_generate():
+    word = input("Enter a word:").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except:
+        print("Sorry, Only letters in the alphabet please!!!")
+        phonetic_generate()
+    else:
+        print(output_list)
+        
+phonetic_generate()
